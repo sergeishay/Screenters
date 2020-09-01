53270baa3ec491e95616e8f967e2a5a65e80331d
myusername <Matanfriedleet@hotmail.com>
2020-09-01 18:28:09 +0300

    adding server less

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.static(join(__dirname, "build")));
 
-if(process.env.NODE_ENV === 'production') {
-    app.get('/*', function (req, res) {
-         res.sendFile(join(__dirname, 'build', 'index.html'));
-    });
-  }  
+app.get('/*', function (req, res) {
+        res.sendFile(join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
