ecbc65485133e2ada0b7f391bbaab126503618d5
myusername <Matanfriedleet@hotmail.com>
2020-09-01 17:12:52 +0300

    fixes added

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(join(__dirname, "build")));
 
 if(process.env.NODE_ENV === 'production') {
     app.get('/*', function (req, res) {
-         res.sendFile(path.join(__dirname, 'build', 'index.html'));
+         res.sendFile(join(__dirname, 'build', 'index.html'));
     });
   }  
 
