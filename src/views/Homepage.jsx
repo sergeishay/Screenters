259dbdbc2b198b5f6283163083ad49951d869213
Chikoom <callforcopy@gmail.com>
2020-09-02 10:55:35 +0300

    added mdbreact

---
+++ src/views/Homepage.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+import Hero from '../components/Hero'
+import Content from '../components/Content'
+import SCRCarousel from '../components/Carousel/Carousel'
+
+const Homepage = () => (
+  <Fragment>
+    <SCRCarousel />
+  </Fragment>
+)
+
+export default Homepage
