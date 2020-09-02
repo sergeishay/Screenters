6585b57674009bf889be042864e83f8a65230bbe
myusername <Matanfriedleet@hotmail.com>
2020-09-02 12:10:02 +0300

    fixes added to docker

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import { Row, Col } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+
 import contentData from "../utils/contentData";
 
 class Content extends Component {
